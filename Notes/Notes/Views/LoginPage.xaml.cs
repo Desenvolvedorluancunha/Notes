915b8545956bf34cf310c485e2b467d0d766b571
Luan Cunha <luan.cunha@eosconsultores.com.br>
2022-07-03 18:45:09 -0400

    Adicionar arquivos de projeto.

---
+++ Notes/Notes/Views/LoginPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Logar(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NotesPage());
+        }
+    }
+}
